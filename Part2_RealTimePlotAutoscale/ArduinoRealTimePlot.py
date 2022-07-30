7f8670213146282696a4f710a2c09f60364ff80d
WaveShapePlay <WaveShapePlay@gmail.com>
2022-07-30 10:07:24 -0400

    Add files via upload
    Initial Commit

---
+++ Part2_RealTimePlotAutoscale/ArduinoRealTimePlot.py
@@ -0,0 +1,76 @@
+import time
+import serial
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+from matplotlib.widgets import RadioButtons
+
+class AnimationPlot:
+
+    def __init__(self):
+        self.enableAutoscale = False
+        
+    def animate(self,i, dataList, ser):
+            ser.write(b'g')                                     # Transmit the char 'g' to receive the Arduino data point
+            arduinoData_string = ser.readline().decode('ascii') # Decode receive Arduino data as a formatted string
+            #print(i)                                           # 'i' is a incrementing variable based upon frames = x argument
+
+            try:
+                arduinoData_float = float(arduinoData_string)   # Convert to float
+                dataList.append(arduinoData_float)              # Add to the list holding the fixed number of points to animate
+
+            except:                                             # Pass if data point is bad                               
+                pass
+
+            dataList = dataList[-50:]                           # Fix the list size so that the animation plot 'window' is x number of points
+
+            ax.clear()                                          # Clear last data frame
+            ax.plot(dataList)                                   # Plot new data frame
+            self.getTitle()
+            
+                                                                # Set Y axis limit of plot
+            
+            if self.enableAutoscale == True:
+                ax.autoscale()
+            if self.enableAutoscale == False:
+                ax.set_ylim([0, 1200])
+                
+
+    def getRadioSelect(self,selection):
+            
+            if selection == "Autoscale":
+                self.enableAutoscale = True
+            else:
+                self.enableAutoscale = False
+
+    def getTitle(self):
+            ax.set_title("Arduino Data")                        # Set title of figure
+            ax.set_ylabel("Value")                              # Set title of y axis
+
+
+   
+dataList = []                                           # Create empty list variable for later use
+                                                        
+fig = plt.figure()                                      # Create Matplotlib plots fig is the 'higher level' plot window
+ax = fig.add_subplot(111)                               # Add subplot to main fig window
+plt.subplots_adjust(left=0.3)
+
+realTimePlot = AnimationPlot()
+
+axcolor = 'lightgoldenrodyellow'
+rax = plt.axes([0.0, 0.0, 0.15, 0.30],facecolor=axcolor)
+radioAutoScale = RadioButtons(rax, ['Fixed','Autoscale'],activecolor='b')
+radioAutoScale.on_clicked(realTimePlot.getRadioSelect)
+
+
+ser = serial.Serial("COM7", 9600)                       # Establish Serial object with COM port and BAUD rate to match Arduino Port/rate
+time.sleep(2)                                           # Time delay for Arduino Serial initialization 
+
+
+
+                                                        # Matplotlib Animation Fuction that takes takes care of real time plot.
+                                                        # Note that 'fargs' parameter is where we pass in our dataList and Serial object. 
+ani = animation.FuncAnimation(fig, realTimePlot.animate, frames=100, fargs=(dataList, ser), interval=30) 
+
+plt.show()                                              # Keep Matplotlib plot persistent on screen until it is closed
+ser.close()                                             # Close Serial connection when plot is closed
+
